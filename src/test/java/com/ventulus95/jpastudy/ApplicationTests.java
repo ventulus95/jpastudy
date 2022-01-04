52b2bfbb4402c20f138c3864c346d119bf0034e4
이창섭 <ventulus95@gmail.com>
2022-01-04 09:28:55 +0900

    ✨ 프로젝트 설정

---
+++ src/test/java/com/ventulus95/jpastudy/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventulus95.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
